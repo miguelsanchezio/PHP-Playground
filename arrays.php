2828338df645d475d290e8221900620029874e48
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-01-18 13:56:33 -0800

    Played with arrays, math, and variables

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$numberList = array(12, 13, 123, '412', 42, 12, '123', '<h1>Hello</h1>');
+
+print_r($numberList);
+
+// echo $numberList[0];
+
+?>
+    
+</body>
+</html>
---
+++ math.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo 56 + 45 * 21 / 32 -2;
+
+?>
+    
+</body>
+</html>
